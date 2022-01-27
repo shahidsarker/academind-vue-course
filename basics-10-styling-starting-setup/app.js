2384092a2f10ca61d290c746fb785219b4be115c
Shahid Sarker <shahid.sarker@gmail.com>
2022-01-27 14:15:20 -0500

    dynamic styling

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -6,11 +6,16 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     boxSelected(box) {
-      if (box === "A") this.boxASelected = true;
-      else if (box === "B") this.boxBSelected = true;
-      else if (box === "C") this.boxCSelected = true;
+      if (box === "A") this.boxASelected = !this.boxASelected;
+      else if (box === "B") this.boxBSelected = !this.boxBSelected;
+      else if (box === "C") this.boxCSelected = !this.boxCSelected;
     },
   },
 });
