89757e568df2a1c3a01735b27c6b5d8f3ee16163
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-03 14:39:18 -0500

    game over display

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -48,6 +49,26 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // draw
+        this.winner = "draw";
+    } else if (value <= 0) {
+        // player lost
+        this.winner = "monster";
+    }
+},
+monsterHealth(value) {
+    if (value <= 0 && this.playerHealth <= 0) {
+        // draw
+        this.winner = "draw";
+    } else if (value <= 0) {
+        // monster lost
+        this.winner = "player";
+      }
     },
   },
 });
