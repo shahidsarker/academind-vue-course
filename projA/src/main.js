e6da33f1eb2327cd8c54e3409fa88fda6d28db7c
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-04 14:18:33 -0500

    friend contact component init

--- projA/src/main.js
+++ projA/src/main.js
@@ -1,3 +1,9 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import App from "./App";
+import FriendContact from "./components/FriendContact";
 
-createApp({}).mount('');
+const app = createApp(App);
+
+app.component("friend-contact", FriendContact);
+
+app.mount("#app");
