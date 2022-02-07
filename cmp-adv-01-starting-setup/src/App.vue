c989d9be6f5b8f9d3b6ec72ed315d5aad697fbe3
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-07 15:10:13 -0500

    local components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  components: {
+    BadgeList,
+    TheHeader,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
