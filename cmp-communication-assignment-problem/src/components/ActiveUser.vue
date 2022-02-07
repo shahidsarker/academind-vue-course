fecb8c957c1ea98393c3bb0e681efdd106f92b36
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-07 11:11:22 -0500

    active user user data assignment

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>username: {{ username }}</h2>
+    <h3>age: {{ age }}</h3>
+  </div>
+</template>
+
+<script>
+// ActiveUser should output a username (h2) and age (h3)
+export default { props: ["username", "age"] };
+</script>
+
+<style></style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">username: </label>
+      <input type="text" v-model="username" />
+    </div>
+
+    <div>
+      <label for="age">age: </label>
+      <input type="number" v-model="age" />
+    </div>
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      age: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit("add-user", this.username, this.age);
+      this.username = "";
+      this.age = "";
+    },
+  },
+};
+</script>
+
+<style></style>
