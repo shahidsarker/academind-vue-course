2e28470e975ec5dffcf9fa6ad887fb7d31f37a19
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-07 16:56:26 -0500

    dynamic components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,42 +1,39 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
-    <UserInfo
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    />
-    <CourseGoals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-      </template>
-    </CourseGoals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'" />
+    <ManageGoals v-if="selectedComponent === 'manage-goals'" /> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
-import BadgeList from "./components/BadgeList.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals";
 
 export default {
   components: {
-    BadgeList,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
     TheHeader,
-    UserInfo,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
