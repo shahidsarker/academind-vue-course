362293e60787bdba9de10fbef69707883766c62d
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-21 16:02:20 -0500

    route metadata auth

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -16,6 +16,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      meta: { needsAuth: true },
       components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
@@ -47,7 +48,10 @@ const router = createRouter({
 router.beforeEach(function (to, from, next) {
   console.log('Global beforeEach');
   console.log(to, from);
-  next();
+  if (to.meta.needsAuth) {
+    console.log('needs auth');
+    next();
+  } else next();
 });
 
 router.afterEach(function (to, from) {
