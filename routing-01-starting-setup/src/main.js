15154d07548cfa57d942aacf7e8b10fa0568f4e6
Shahid Sarker <shahid.sarker@gmail.com>
2022-02-21 10:28:38 -0500

    router-view

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/teams', component: 'TeamsList' }, {}],
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
 });
 
 const app = createApp(App);
